32437062a0c2868c178ccb2e5483ca5f4f34058c
Ivan-PC\Ivan <kimagure44@yahoo.es>
2017-09-13 00:48:03 +0200

    Proyectos con YeoMan

---
+++ myAppAngular/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myAppAngularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the myAppAngularApp
+ */
+angular.module('myAppAngularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ myAppAngular/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myAppAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the myAppAngularApp
+ */
+angular.module('myAppAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
